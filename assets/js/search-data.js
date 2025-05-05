04e1317ec5b0761e74ea2fcaf90f742a4b3cbbb8
zachdoesneuro <zachdoesneuro@proton.me>
2025-05-05 19:09:24 +0000

    Deploying to gh-pages from @ zachdoesneuro/zachdoesneuro.github.io@dd2c1a0c394bb3a03acea7c609b857b4460f3ed1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,7 +479,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%5A.%53%74%72%69%63%6B%6C%61%6E%64@%75%66%6C.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
